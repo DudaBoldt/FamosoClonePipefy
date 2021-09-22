a7bf2c094655cf4e9c7a3de01d2cd4a1b26161eb
Maria Eduarda Boldt <edraude123@gmail.com>
2021-09-22 18:16:00 -0300

    Inicio

---
+++ src/components/Card/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useDrag } from 'react-dnd';
+import {Container, Label} from './styles';
+
+export default function Card({ data }) {
+  const [{isDragging}, dragRef] = useDrag({
+    type: "CARD",
+    collect: monitor => ({
+      isDragging: !!monitor.isDragging()
+    })
+  })
+
+  return (
+    <Container ref={dragRef} isDragging={isDragging}>
+      <header>
+        {data.labels.map(label => <Label key={label} color={label} title='Tooltip'/>)}
+      </header>
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt="" />}
+    </Container>
+  )
+}
